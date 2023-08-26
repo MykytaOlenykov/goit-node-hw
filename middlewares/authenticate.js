35ae19af0bf8cf376d94836a203e996d24f5c6a1
Mykyta Olenykov <nikita2001395@gmail.com>
2023-08-26 10:21:13 +0300

    fix: authenticate middlewares

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -10,7 +10,7 @@ const authenticate = async (req, _, next) => {
     const { authorization = "" } = req.headers;
     const [bearer, token] = authorization.split(" ");
 
-    if (bearer !== "Bearer") {
+    if (bearer !== "Bearer" || !token) {
       throw HttpError(401);
     }
 
