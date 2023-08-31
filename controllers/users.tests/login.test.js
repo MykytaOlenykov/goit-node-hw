f9a9a48b9fc12d65c719c65aecaf8faf036897c0
Mykyta Olenykov <nikita2001395@gmail.com>
2023-08-31 21:20:13 +0300

    add: hw05

---
+++ controllers/users.tests/login.test.js
@@ -0,0 +1,51 @@
+/* 
+test login controller
+
+1. відповідь повина мати статус-код 200
+2. у відповіді повинен повертатися токен
+3. у відповіді повинен повертатися об'єкт user з 2 полями email и subscription з типом даних String
+*/
+
+const request = require("supertest");
+
+const { connect, disconnect } = require("../../server");
+const app = require("../../app");
+
+const { TEST_EMAIL, TEST_PASSWORD } = process.env;
+
+const credentials = { email: TEST_EMAIL, password: TEST_PASSWORD };
+
+describe("test login controller", () => {
+  beforeAll(() => {
+    connect();
+  });
+
+  afterAll(() => {
+    disconnect();
+  });
+
+  it("відповідь повина мати статус-код 200", async () => {
+    const { statusCode } = await request(app)
+      .post("/api/users/login")
+      .send(credentials);
+
+    expect(statusCode).toBe(200);
+  });
+
+  it("у відповіді повинен повертатися токен", async () => {
+    const { body } = await request(app)
+      .post("/api/users/login")
+      .send(credentials);
+
+    expect(body).toHaveProperty("token");
+  });
+
+  it("у відповіді повинен повертатися об'єкт user з 2 полями email и subscription з типом даних String", async () => {
+    const { body } = await request(app)
+      .post("/api/users/login")
+      .send(credentials);
+
+    expect(typeof body.user.email).toBe("string");
+    expect(typeof body.user.subscription).toBe("string");
+  });
+});
